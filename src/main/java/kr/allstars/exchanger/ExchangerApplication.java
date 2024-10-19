f61a7a7b2869e31c32b42bff1157ed0660ccaffe
TAESEONG KWON <taeseong@me.com>
2024-10-19 20:43:10 +0900

    initial commit

---
+++ src/main/java/kr/allstars/exchanger/ExchangerApplication.java
@@ -0,0 +1,13 @@
+package kr.allstars.exchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangerApplication.class, args);
+	}
+
+}
